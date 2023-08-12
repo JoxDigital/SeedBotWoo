4f14a9fbce602d9eaf7eeb42342f5423a58db001
Qev254 <kelvinkkaranja@gmail.com>
2023-08-12 04:44:10 +0300

    ORE AVATAR Stff

--- includes/seedbot-chat-interface.php
+++ includes/seedbot-chat-interface.php
@@ -8,7 +8,7 @@ ob_start();
     <div id="seedbot-chat-container" class="seedbot-chat-container">
         <div class="seedbot-chat-header" id="seedbot-chat-header">
             <div id="seedbot-title">
-                <div id="seedbot-avatar">
+                <div id="seedbot-avatar" class="seedbot-avatar">
                     <img src="https://dev.joxdigital.com/wp-content/uploads/2023/08/seedbotavatar-scaled-1.png" alt="ChatBot Avatar"/>
                 </div>
                 <div id="seedbot-name"></div>
