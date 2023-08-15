e1fb565cf75b076baf965f21d04106411a75e5af
Qev254 <karanjaqev@gmail.com>
2023-08-15 17:22:59 +0300

    da vinci

--- includes/seedbot-chatbot-processing.php
+++ includes/seedbot-chatbot-processing.php
@@ -12,15 +12,13 @@ if (!empty($user_message)) {
     $api_key = get_option('seedbot_api_key'); // Make sure you have a function to retrieve the API key
 
     // API endpoint
-    $api_url = 'https://api.openai.com/v1/chat/completions';
+    $api_url = 'https://api.openai.com/v1/completions';
 
     // Prepare the data for the API request
     $data = array(
-        'messages' => array(
-            array('role' => 'user', 'content' => $user_message)
-        ),
-        'model' => 'davinci', // Use 'davinci' model for chat completions
-        'max_tokens' => 50 // Adjust as needed
+        'prompt' => $user_message,
+        'max_tokens' => 50, // Adjust as needed
+        'model' => 'davinci-codex' // Use 'davinci-codex' model for chat completions
     );
 
     // Initialize cURL session
@@ -48,8 +46,8 @@ if (!empty($user_message)) {
         var_dump($response_data);
 
         // Extract the chatbot's reply from the response and return it
-        $chatbot_reply = isset($response_data['choices'][0]['message']['content'])
-            ? $response_data['choices'][0]['message']['content']
+        $chatbot_reply = isset($response_data['choices'][0]['text'])
+            ? $response_data['choices'][0]['text']
             : 'Chatbot encountered an issue.';
 
         echo $chatbot_reply;
