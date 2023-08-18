2af0b010452388e49798f621235bee89dd30b059
Qev254 <kelvinkkaranja@gmail.com>
2023-08-18 04:26:52 +0300

    Update seedbot-chat-interface.php

--- includes/seedbot-chat-interface.php
+++ includes/seedbot-chat-interface.php
@@ -4,28 +4,26 @@
 // Start output buffering to capture any output
 ob_start();
 ?>
+
 <div id="seedbot-chat-interface" class="seedbot-chat-interface">
     <div id="seedbot-chat-container" class="seedbot-chat-container">
         <div class="seedbot-chat-header" id="seedbot-chat-header">
             <div id="seedbot-avatar" class="seedbot-avatar">
-                    <img src="https://dev.joxdigital.com/wp-content/uploads/2023/08/seedbotavatar-scaled-1.png" alt="ChatBot Avatar"/>
-                </div>
+                <img src="https://dev.joxdigital.com/wp-content/uploads/2023/08/seedbotavatar-scaled-1.png" alt="ChatBot Avatar"/>
+            </div>
             <div id="seedbot-chat-header-inner">
-            <div id="seedbot-title-container">
-            <div id="seedbot-title">
-                
-                <div id="seedbot-name">
-                    <h3>SeedBot GPT-4</h3>
-                    <P>Seedconnect's AI Customer Assistant
+                <div id="seedbot-title-container">
+                    <div id="seedbot-title">
+                        <div id="seedbot-name">
+                            <h3>SeedBot GPT-4</h3>
+                            <p>Seedconnect's AI Customer Assistant</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            </div>
-            <div id="seedbot-options" class="seedbot-options">
-                <div class="seedbot-option">Cart</div>
-                <div class="seedbot-option">Recently Viewed Products </div>
-                <div class="seedbot-option">Order Tracking</div>
-                <div class="seedbot-option">Preferences</div>
-            </div>
+                <div id="seedbot-options" class="seedbot-options">
+                    <div class="seedbot-option">Products</div>
+                    <div class="seedbot-option">Orders</div>
+                </div>
             </div>
         </div>
         <div class="seedbot-chat-messages" id="seedbot-chat-messages">
@@ -40,9 +38,9 @@ ob_start();
                 <!-- Emojis come here  -->
             </div>
         </div>
-
     </div>
 </div>
+
 
 <?php
 // Get the buffered content and send it to the browser
